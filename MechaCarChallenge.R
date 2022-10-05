e84a0f0eaaba0c29d3396d764e56f884d7e9b616
MarckBoge <90155651+MarckBoge@users.noreply.github.com>
2022-10-04 23:18:16 -0400

    update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,4 @@
-########Deliverable 1#######
+###########Deliverable 1##########
 # load package
 library(dplyr)
 # Import and read csv as dataframe
@@ -9,4 +9,11 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # summarize linear model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
 
+###########Deliverable 2##########
+# Import and read csv as dataframe
+Suspension_Coil <- read.csv(file = 'Suspension_Coil.csv',check.names = F, stringsAsFactors = F)
+# Create total_summary dataframe using summarize function
+total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# Create lot_summary dataframe using group_by and summarize functions
+lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI), .groups = 'keep')
 
